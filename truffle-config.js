d0f4e1170c4d6ce87991772d7acb51378527cd4a
matprime <matt@mattprimeonline.com>
2020-09-25 12:07:45 +0200

    config for Matic testnet

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,8 @@ require('babel-register');
 require('babel-polyfill');
 
 var HDWalletProvider = require("truffle-hdwallet-provider");
-const MNEMONIC = "a86530b58be1b140ff8e75669ee6d4d04b614f31bd74cc341b948f602a1b8e92";
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
@@ -11,13 +12,13 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(MNEMONIC, "https://ropsten.infura.io/v3/53e15db164ab4584b98c0c774bb82b50")
-      },
-      network_id: 3,
-      gas: 4000000
-    }
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
